8cee9dbbfc839f45aedceef9312f6a3aed817f94
Your Name <you@example.com>
2017-09-30 16:47:37 +0800

    enabled map

--- docs/js/main.js
+++ docs/js/main.js
@@ -284,41 +284,51 @@ jQuery(document).ready(function(){
 	  	return ((minutes < 0) ? 0 : minutes);;
 	}
 
-	/*
-	function initMap() {
-		var map = new google.maps.Map(document.getElementById('map'), {
-		  zoom: 8,
-		  center: {lat: 40.731, lng: -73.997}
-		});
-		var geocoder = new google.maps.Geocoder;
-		var infowindow = new google.maps.InfoWindow;
+      // Note: This example requires that you consent to location sharing when
+      // prompted by your browser. If you see the error "The Geolocation service
+      // failed.", it means you probably did not give permission for the browser to
+      // locate you.
+      var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 18
+        });
+        infoWindow = new google.maps.InfoWindow;
 
-		document.getElementById('submit').addEventListener('click', function() {
-		  geocodeLatLng(geocoder, map, infowindow);
-		});
-	}
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
 
-	function geocodeLatLng(geocoder, map, infowindow) {
-		var input = document.getElementById('latlng').value;
-		var latlngStr = input.split(',', 2);
-		var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-		geocoder.geocode({'location': latlng}, function(results, status) {
-		  if (status === 'OK') {
-		    if (results[0]) {
-		      map.setZoom(11);
-		      var marker = new google.maps.Marker({
-		        position: latlng,
-		        map: map
-		      });
-		      infowindow.setContent(results[0].formatted_address);
-		      infowindow.open(map, marker);
-		    } else {
-		      window.alert('No results found');
-		    }
-		  } else {
-		    window.alert('Geocoder failed due to: ' + status);
-		  }
-		});
-	}
-	*/
+            console.log('My lat: ' + position.coords.latitude);
+            console.log('My lng: ' + position.coords.longitude);
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+
+
+          
+
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
 });
